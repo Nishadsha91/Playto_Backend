503a99a5ec6e6e8d04a63d862b00d3203bc0d798
Nishadsha91 <muthumattilnishad91@gmail.com>
2026-04-28 19:00:41 +0530

    Fix database config for Render

--- config/settings.py
+++ config/settings.py
@@ -98,10 +98,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600
     )
 }
 
